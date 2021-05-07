04fd92c96e0bbaf32e88e14d08ddd74498a3bf00
MohamedDib <49766520+MohamedDib@users.noreply.github.com>
2021-05-07 14:28:49 +0200

    2nd test

--- PasswordProject/src/main/java/PasswordValidator.java
+++ PasswordProject/src/main/java/PasswordValidator.java
@@ -1,17 +1,10 @@
 public class PasswordValidator {
 
-    public String getPassword() {
-        return password;
-    }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
     private String password;
 
-    public PasswordValidator(String password){
-        this.password = password;
+    public PasswordValidator(){
     }
 
     public void verifyPassword(){
@@ -22,7 +15,45 @@ public class PasswordValidator {
         // at least one number
     }
 
+    public boolean hasMoreThan6Car(String password){
+        if (password.length()>=6)
+        {
+            return true;
+        }else {
+            return false;
+        }
+    }
 
+    public boolean hasAtLeastOneChar(String password){
+        String lowerCaseChars = "(.*[a-z].*)";
+        String upperCaseChars = "(.*[A-Z].*)";
+
+        if (!password.matches(lowerCaseChars ) || !password.matches(upperCaseChars ))
+        {
+            System.out.println("Password must have atleast one lowercase character");
+            return false;
+        }else{
+            return true;
+        }
+
+
+    }
+    public boolean hasAtLeastOneNum(String password){
+        if (password.length()>=6)
+        {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
 
 }
--- PasswordProject/src/test/java/PasswordValidatorTest.java
+++ PasswordProject/src/test/java/PasswordValidatorTest.java
@@ -1,17 +1,25 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PasswordValidatorTest {
 
+    PasswordValidator pv = new PasswordValidator();
+
     @Test
     void testHasMoreThan6Car(){
 
+        String pass = "TestPassword";
+
+        assertTrue(pv.hasMoreThan6Car(pass));
     }
 
     @Test
     void testHasAtLeastOneChar(){
+        String pass = "TestPassword";
 
+        assertTrue(pv.hasMoreThan6Car(pass));
     }
 
     @Test
