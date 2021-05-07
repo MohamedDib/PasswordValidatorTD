4b9010b33a0dd51bbee1441db1f69aaeca8dbbf3
MohamedDib <49766520+MohamedDib@users.noreply.github.com>
2021-05-07 13:58:37 +0200

    password validator + conf

--- PasswordProject/src/main/java/PasswordValidator.java
+++ PasswordProject/src/main/java/PasswordValidator.java
@@ -1,2 +1,52 @@
 public class PasswordValidator {
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String password;
+
+    public PasswordValidator(String password){
+        this.password = password;
+    }
+
+    public void verifyPassword(){
+        // more than 6 caracters
+
+        // at least one char
+
+        // at least one number
+    }
+
+    private boolean hasMoreThan6Car(String password){
+        if (password.length()>=6)
+        {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    private boolean hasAtLeastOneChar(String password){
+        if (password.length()>=6)
+        {
+            return true;
+        }else {
+            return false;
+        }
+    }
+    private boolean hasAtLeastOneNum(String password){
+        if (password.length()>=6)
+        {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+
 }
